4e26e797714fc248c8443c316885991008d2be7e
Romerio <romerio.unix@gmail.com>
2018-09-30 22:20:53 -0300

    api apontando para o heroku

--- app/api/Routes.js
+++ app/api/Routes.js
@@ -2,7 +2,7 @@ import { Constants } from 'expo'
 
 //const baseUrl = Constants.appOwnership === 'expo' ? 'http://192.168.0.5:3000' : 'https://auth-back.herokuapp.com'
 //const baseUrl = 'https://auth-back.herokuapp.com'
-const baseUrl = 'http://192.168.0.5:3000'
+const baseUrl = 'https://auth-back.herokuapp.com'
 
 export default {
     LOGIN: {
